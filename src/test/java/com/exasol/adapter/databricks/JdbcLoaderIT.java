d27c9cee7f97c7d4423f444e5ff3f2bf8e51ec37
Christoph Pirkl <christoph.pirkl@exasol.com>
2024-09-03 11:24:04 +0200

    Code cleanup

--- src/test/java/com/exasol/adapter/databricks/JdbcLoaderIT.java
+++ src/test/java/com/exasol/adapter/databricks/JdbcLoaderIT.java
@@ -10,12 +10,15 @@ import com.exasol.dbbuilder.dialects.exasol.ConnectionDefinition;
 class JdbcLoaderIT extends AbstractIntegrationTestBase {
     @Test
     void importFromJdbcWorks() {
-        final ConnectionDefinition databricksConnection = testSetup.exasol().getConnectionDefinition();
         final Table table = testSetup.databricks().createSchema().createTable("tab", "id", "INT", "name", "STRING")
                 .insert(1, "a").insert(2, "b").insert(3, "c");
         final String databricksQuery = "select * from " + table.getFullyQualifiedName() + " order by id";
-        final String query = "IMPORT FROM JDBC AT " + databricksConnection.getName() + " STATEMENT '" + databricksQuery
-                + "'";
+        final String query = "IMPORT FROM JDBC AT " + getConnectionName() + " STATEMENT '" + databricksQuery + "'";
         testSetup.exasol().assertions().query(query, table().row(1, "a").row(2, "b").row(3, "c").matches());
+    }
+
+    private String getConnectionName() {
+        final ConnectionDefinition databricksConnection = testSetup.exasol().getConnectionDefinition();
+        return databricksConnection.getName();
     }
 }
