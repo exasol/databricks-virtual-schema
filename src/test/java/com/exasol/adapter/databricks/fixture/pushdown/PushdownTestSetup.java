b49c507239a54c1eeca54eb2df828b8203a29daa
Christoph Pirkl <christoph.pirkl@exasol.com>
2024-09-10 09:52:56 +0200

    Refactor integration tests

---
+++ src/test/java/com/exasol/adapter/databricks/fixture/pushdown/PushdownTestSetup.java
@@ -0,0 +1,53 @@
+package com.exasol.adapter.databricks.fixture.pushdown;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.*;
+
+import com.exasol.adapter.databricks.databricksfixture.DatabricksSchema;
+import com.exasol.adapter.databricks.fixture.TestSetup;
+import com.exasol.dbbuilder.dialects.Table;
+import com.exasol.dbbuilder.dialects.exasol.VirtualSchema;
+
+public class PushdownTestSetup {
+
+    private final TestSetup testSetup;
+    private final List<TableFactory> tableFactories;
+
+    private PushdownTestSetup(final TestSetup testSetup, final List<TableFactory> tableFactories) {
+        this.testSetup = testSetup;
+        this.tableFactories = tableFactories;
+    }
+
+    public static PushdownTestSetup create(final TestSetup testSetup, final List<TableFactory> tableFactories) {
+        return new PushdownTestSetup(testSetup, tableFactories);
+    }
+
+    private PushdownTestFactory createTestFactory() {
+        final DatabricksSchema databricksSchema = testSetup.databricks().createSchema();
+        final List<Table> databricksTables = createDatabricksTables(databricksSchema);
+        final VirtualSchema virtualSchema = testSetup.exasol().createVirtualSchema(databricksSchema);
+        return new PushdownTestFactory(testSetup, virtualSchema, databricksTables);
+    }
+
+    private List<Table> createDatabricksTables(final DatabricksSchema databricksSchema) {
+        return tableFactories.stream().map(factory -> factory.createTable(databricksSchema)).toList();
+    }
+
+    public Stream<DynamicNode> buildTests(final String testCategory, final TestBuilder testBuilder) {
+        final Stream<DynamicTest> tests = testBuilder.buildTests(createTestFactory())
+                .map(PushdownTestHolder::toDynamicTest);
+        return Stream.of(DynamicContainer.dynamicContainer(testCategory, tests));
+    }
+
+    @FunctionalInterface
+    public static interface TestBuilder {
+        Stream<PushdownTestHolder> buildTests(PushdownTestFactory factory);
+    }
+
+    @FunctionalInterface
+    public static interface TableFactory {
+        Table createTable(DatabricksSchema databricksSchema);
+    }
+}
